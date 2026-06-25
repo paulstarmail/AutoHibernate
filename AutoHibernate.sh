bdcc312c5f59234111834442ba28be36ba79a87d
Amal K Paul <60135524+paulstarmail@users.noreply.github.com>
2026-06-25 10:23:01 +0530

    Update AutoHibernate.sh

--- AutoHibernate.sh
+++ AutoHibernate.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Infinite loop to check the battery percentage every minute
+# Ensure the script can find system binaries in the cron/systemd environment
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 while true; do
-    # Get battery device path
     battery_device=$(upower -e | grep 'BAT')
 
-    # Get the battery percentage and state
-    battery_percentage=$(upower -i "$battery_device" | grep -i 'percentage' | awk '{print $2}' | sed 's/%//')
-    battery_state=$(upower -i "$battery_device" | grep -i 'state' | awk '{print $2}')
+    if [[ -n "$battery_device" ]]; then
+        battery_percentage=$(upower -i "$battery_device" | grep -i 'percentage' | awk '{print $2}' | sed 's/%//')
+        battery_state=$(upower -i "$battery_device" | grep -i 'state' | awk '{print $2}')
 
-    # Check if battery is not charging and percentage is 8 or less
-    if [[ -n "$battery_percentage" && "$battery_percentage" -le 8 && "$battery_state" == "discharging" ]]; then
-        sudo systemctl hibernate
+        if [[ -n "$battery_percentage" && "$battery_percentage" -le 8 && "$battery_state" == "discharging" ]]; then
+            # No 'sudo' needed here; systemd runs this as root natively
+            systemctl hibernate
+        fi
     fi
 
-    # Sleep for 1 minute (60 seconds) before checking again
     sleep 60
 done
